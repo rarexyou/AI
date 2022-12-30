62f676834b5daf66309e376b8568d27c6d35c02c
rarexyou <121542357+rarexyou@users.noreply.github.com>
2022-12-30 15:02:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-BOT_ID = getenv("BOT_ID", "")#token first 10 digits
-AI_API_KEY = getenv("AI_API_KEY", "")
-AI_ID = int(getenv("AI_ID", ""))
+BOT_TOKEN = getenv("BOT_TOKEN", "5817576023:AAHTsumNeEjJCFuD7guGK1DvgqjIxMCE7uQ")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://yashraikwar:<#Yash5123>@cluster0.hfrpo2o.mongodb.net/?retryWrites=true&w=majority")
+BOT_ID = getenv("BOT_ID", "5817576023")#token first 10 digits
+AI_API_KEY = getenv("AI_API_KEY", "uQGcpT0PhmidoZSi")
+AI_ID = int(getenv("AI_ID", "171521"))
